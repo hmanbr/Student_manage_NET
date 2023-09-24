cfb0fa02955e214aa0e382bb6f69e58e53ef4957
key <dev.anhtu@gmail.com>
2023-09-24 10:02:07 +0700

    feat: add sesion

--- G3/Program.cs
+++ G3/Program.cs
@@ -8,16 +8,24 @@ builder.Services.AddDbContext<SWPContext>(options => new SWPContext());
 builder.Services.AddSingleton<IMailService, MailService>();
 builder.Services.AddSingleton<IHashService, HashService>();
 
+builder.Services.AddSession(options =>
+{
+    options.Cookie.Name = "SessionCookie";
+    options.IdleTimeout = TimeSpan.FromDays(1);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
 var app = builder.Build();
 
-if (!app.Environment.IsDevelopment()) {
+if (!app.Environment.IsDevelopment())
+{
     app.UseExceptionHandler("/Home/Error");
     app.UseHsts();
 }
-
+app.UseSession();
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 
 app.UseAuthorization();
 app.MapControllers();
