fa6ff11d48d93394e7f41242bcf950c3db0316cb
key <dev.anhtu@gmail.com>
2023-09-26 10:59:00 +0700

    chore: change ui

--- G3/Controllers/AdminController.cs
+++ G3/Controllers/AdminController.cs
@@ -12,6 +12,7 @@ namespace G3.Controllers
             _context = context;
         }
 
+        [Route("/")]
         [Route("/Admin/AdminHome")]
         public IActionResult AdminHome()
         {
