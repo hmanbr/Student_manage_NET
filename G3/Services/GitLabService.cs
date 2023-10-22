664e5162a40716f111ec1c8d0d89a6527a0a460e
Dev <dev.anhtu@gmail.com>
2023-10-23 03:54:01 +0700

    chore: gitlab

--- G3/Services/GitLabService.cs
+++ G3/Services/GitLabService.cs
@@ -4,43 +4,21 @@ using NGitLab.Models;
 
 namespace G3.Services
 {
-    public class GitLabService
+    public class GitLabService: IGitLabService
     {
         private readonly IConfiguration Configuration;
         public GitLabClient client;
-        public SWPContext context;
 
-        public GitLabService(IConfiguration configuration, SWPContext context)
+        public GitLabService(IConfiguration configuration)
         {
             Configuration = configuration;
             client = new GitLabClient("https://gitlab.com", Configuration["GitLab:PrivateToken"]);
-            this.context = context;
         }
 
-        public void CreateProject(int ProjectId, string projectCode, string englishName, string vietnameseName, string description, int mentorId, int leaderId, string groupName, bool isActive = true)
+        public List<NGitLab.Models.Milestone> GetMilestoneByGroupId(int groupId)
         {
-            //
-        }
-
-        public void GetMilestones(List<int> ProjectIds)
-        {
-            foreach (var projectId in ProjectIds)
-            {
-                var milestoneClient = client.GetMilestone(projectId: projectId);
-                List<NGitLab.Models.Milestone> milestones = milestoneClient.All.ToList();
-            }
-        }
-
-        public NGitLab.Models.Milestone CreateMilestone(int projectId, string title, string description, DateTime dueDate, DateTime startDate)
-        {
-            var milestone = client.GetMilestone(projectId: projectId).Create(new NGitLab.Models.MilestoneCreate
-            {
-                Title = title,
-                Description = description,
-                StartDate = startDate.ToString("yyyy-MM-dd HH:mm:ss"),
-                DueDate = dueDate.ToString("yyyy-MM-dd HH:mm:ss")
-            });
-            return milestone;
+            IMilestoneClient milestoneClient = client.GetGroupMilestone(groupId);
+            return milestoneClient.All.ToList();
         }
     }
 }
--- G3/Services/IGitLabService.cs
+++ G3/Services/IGitLabService.cs
@@ -5,10 +5,8 @@ namespace G3.Services
     public interface IGitLabService
     {
 
-        public Group CreateGroup(string className, string description);
-        public void CreateProject(int ProjectId, string projectCode, string englishName, string vietnameseName, string description, int mentorId, int leaderId, string groupName, bool isActive = true);
-        public void GetMilestones(List<int> ProjectIds);
-        public NGitLab.Models.Milestone CreateMilestone(int projectId, string title, string description, DateTime dueDate, DateTime startDate);
+        public List<NGitLab.Models.Milestone> GetMilestoneByGroupId(int groupId);
+
 
     }
 }
