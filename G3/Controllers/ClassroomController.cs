695b0e71d3a048ae2c3d2eb2a50bdf3e80cd3504
minhnhhe172293 <minhnhhe172293@fpt.edu.vn>
2023-10-22 20:47:20 +0700

    add export excel of studenst in class

--- G3/Controllers/ClassroomController.cs
+++ G3/Controllers/ClassroomController.cs
@@ -1,8 +1,10 @@
-﻿using ExcelDataReader;
+﻿using ClosedXML.Excel;
+using ExcelDataReader;
 using G3.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.ComponentModel;
+using System.Data;
 using System.Text;
 
 namespace G3.Controllers
@@ -41,10 +43,12 @@ namespace G3.Controllers
 		public async Task<IActionResult> ClassDetail(int id)
 		{
 			var usersInClass = await _context.Classes
-								.Where(cls => cls.Id == id)
-								.Include(cls => cls.Users) // Eager loading to load associated users
-								.Select(cls => cls.Users)
-								.FirstOrDefaultAsync();
+							.Where(cls => cls.Id == id)
+							.Include(cls => cls.Users) // Eager loading to load associated users
+							.SelectMany(cls => cls.Users)
+							.Take(5) // Limit to the first 5 users
+							.ToListAsync();
+
 			ViewBag.ClassId = id;
 			return View("/Views/Classroom/ClassDetail.cshtml", usersInClass);
 		}
@@ -52,39 +56,43 @@ namespace G3.Controllers
 		[Route("/ManageClassroom/ClassStudents")]
 		public async Task<IActionResult> ClassStudents(int id)
 		{
-
-				var usersInClass = await _context.Classes
-								.Where(cls => cls.Id == id)
-								.Include(cls => cls.Users) // Eager loading to load associated users
-								.Select(cls => cls.Users.Where(user => user.RoleSettingId == 5))
-								.FirstOrDefaultAsync();
+			var usersInClass = await _context.Classes
+							.Where(cls => cls.Id == id)
+							.Include(cls => cls.Users) // Eager loading to load associated users
+							.Select(cls => cls.Users.Where(user => user.RoleSettingId == 5))
+							.FirstOrDefaultAsync();
 			ViewBag.ClassId = id;
 			return View("/Views/Classroom/ClassStudents.cshtml", usersInClass);
 		}
 
 		[Route("/ManageClassroom/ClassAddStudents")]
-		public async Task<IActionResult> ClassAddStudents()
+		public async Task<IActionResult> ClassAddStudents(int id)
 		{
 			var studentList = await _context.Users.Where(userList => userList.RoleSettingId == 5).ToListAsync();
+			ViewBag.ClassId = id;
 			return View("/Views/Classroom/ClassAddStudents.cshtml", studentList);
 		}
 
+		[Route("ManageClassroom/AddStudentsToClass")]
 		[HttpPost]
-		public async Task<IActionResult> AddStudentsToClass(UserMultiSelectViewModel userMultiSelectViewModel)
+		public async Task<IActionResult> AddStudentsToClass(int stuId, int classId)
 		{
-			List<User> userAddList = new List<User>();
+			User existingUser = _context.Users.FirstOrDefault(u => u.Id == stuId);
 
-			foreach (var selectedUser in userMultiSelectViewModel.UsersSelectList)
+			var classToAddUserTo = await _context.Classes
+				.Where(c => c.Id == classId)
+				.Include(c => c.Users)
+				.FirstOrDefaultAsync();
+
+
+			if (classToAddUserTo != null && !classToAddUserTo.Users.Any(u => u.Email == existingUser.Email))
 			{
-				var selectedUserList = _context.Users.Where(user => user.Id.Equals(selectedUser.User.Id)).FirstOrDefault();
-				if (selectedUserList != null)
-				{
-					userAddList.Add(selectedUserList);
-				}
-
+				// Add the user to the class
+				classToAddUserTo.Users.Add(existingUser);
+				await _context.SaveChangesAsync(); // Save the class-user relationship
 			}
-			IEnumerable<User> UserAddListEnu = userAddList;
-			return View("/Views/Classroom/ClassStudents.cshtml", UserAddListEnu);
+			ViewBag.ClassId = classId;
+			return RedirectToAction("ClassStudents", "Classroom", new { id = classId });
 		}
 
 		[Route("/ManageClassroom/UploadExcel")]
@@ -100,10 +108,10 @@ namespace G3.Controllers
 		{
 			Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
 
-			if(file != null && file.Length > 0)
+			if (file != null && file.Length > 0)
 			{
 				var uploadFolder = $"{Directory.GetCurrentDirectory()}\\wwwroot\\Uploads";
-				if(!Directory.Exists(uploadFolder))
+				if (!Directory.Exists(uploadFolder))
 				{
 					Directory.CreateDirectory(uploadFolder);
 				}
@@ -126,7 +134,7 @@ namespace G3.Controllers
 							bool isHeaderSkipped = false;
 							while (reader.Read())
 							{
-								if(!isHeaderSkipped)
+								if (!isHeaderSkipped)
 								{
 									isHeaderSkipped = true;
 									continue;
@@ -134,9 +142,11 @@ namespace G3.Controllers
 								User user = new User();
 								user.Email = reader.GetValue(0).ToString();
 
-								if(user.Email.Contains("gmail.com")) {
+								if (user.Email.Contains("gmail.com"))
+								{
 									user.DomainSettingId = 7;
-								}else if(user.Email.Contains("fpt.edu.vn"))
+								}
+								else if (user.Email.Contains("fpt.edu.vn"))
 								{
 									user.DomainSettingId = 6;
 								}
@@ -144,44 +154,41 @@ namespace G3.Controllers
 								user.Name = reader.GetValue(1).ToString();
 								user.RoleSettingId = 5;
 								user.Status = true;
-								if(reader.GetValue(2).ToString().Equals("Male"))
+								if (reader.GetValue(2).ToString().Equals("Male"))
 								{
 									user.Gender = true;
-								}else
+								}
+								else
 								{
 									user.Gender = false;
 								}
 
-								bool existed = false;
-								var userList = await _context.Users.ToListAsync();
-								foreach(var existedUser in userList)
-								{
-									if(existedUser.Email.Equals(user.Email))
-									{
-										existed = true;
-									}
-								}
-								
-								if(!existed)
+
+								string emailToCheck = user.Email;
+
+								// Check if the user already exists in the database
+								User existingUser = _context.Users.FirstOrDefault(u => u.Email == emailToCheck);
+
+								if (existingUser == null)
 								{
 									_context.Add(user);
+									await _context.SaveChangesAsync(); // Save the new user
+									existingUser = user; // Update existingUser with the newly added user
 								}
 
 
 								int classId = id;
 								var classToAddUserTo = await _context.Classes
-															.Where(c => c.Id == classId)
-															.Include(c => c.Users)
-															.FirstOrDefaultAsync();
-								string emailToCheck = user.Email;
-								bool userExistsInClass = classToAddUserTo.Users.Any(user => user.Email == emailToCheck);
+									.Where(c => c.Id == classId)
+									.Include(c => c.Users)
+									.FirstOrDefaultAsync();
 
-								if (classToAddUserTo != null && !userExistsInClass)
+								if (classToAddUserTo != null && !classToAddUserTo.Users.Any(u => u.Email == emailToCheck))
 								{
 									// Add the user to the class
-									classToAddUserTo.Users.Add(user);
+									classToAddUserTo.Users.Add(existingUser);
+									await _context.SaveChangesAsync(); // Save the class-user relationship
 								}
-								await _context.SaveChangesAsync();
 							}
 						} while (reader.NextResult());
 					}
@@ -189,6 +196,48 @@ namespace G3.Controllers
 			}
 			ViewBag.ClassId = id;
 			return View("/Views/Classroom/UploadExcel.cshtml");
+		}
+
+		[HttpGet]
+		[Route("/ManageClassroom/ExportExcel")]
+		public async Task<FileResult> ExportStudentsToExcel(int id)
+		{
+			var studentInClass = await _context.Classes
+							.Where(cls => cls.Id == id)
+							.Include(cls => cls.Users) // Eager loading to load associated users
+							.Select(cls => cls.Users.Where(user => user.RoleSettingId == 5))
+							.FirstOrDefaultAsync();
+			var fileName = "ClassID:" + id + ".xlsx";
+			return GenerateExcel(fileName, studentInClass);
+		}
+
+		private FileResult GenerateExcel(string fileName, IEnumerable<User> users)
+		{
+			DataTable dataTable = new DataTable("User");
+			dataTable.Columns.AddRange(new DataColumn[]
+			{
+				new DataColumn("Email"),
+				new DataColumn("Name"),
+				new DataColumn("Gender"),
+			});
+
+			foreach (var user in users)
+			{
+				string genderString = (user.Gender.HasValue) ? (user.Gender.Value ? "Male" : "Female") : "Unknown";
+				dataTable.Rows.Add(user.Email, user.Name, genderString);
+			}
+
+			using (XLWorkbook wb = new XLWorkbook())
+			{
+				wb.Worksheets.Add(dataTable);
+				using(MemoryStream stream = new MemoryStream())
+				{
+					wb.SaveAs(stream);
+					return File(stream.ToArray(),
+						"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+						fileName);
+				}
+			} 
 		}
 	}
 
