ab7e4f89bfb0a18af671c9f3344e9ed04d5fb79a
minhnhhe172293 <minhnhhe172293@fpt.edu.vn>
2023-11-06 20:35:30 +0700

    syncfusion licensing code

--- G3/Program.cs
+++ G3/Program.cs
@@ -4,7 +4,7 @@ using NGitLab;
 var builder = WebApplication.CreateBuilder(args);
 
 Syncfusion.Licensing.SyncfusionLicenseProvider
-    .RegisterLicense("Ngo9BigBOggjHTQxAR8/V1NHaF5cXmVCf1JpRGJGfV5yd0VHYFZTTXxbQE0SNHVRdkdgWH5edHVVRGlYU0RzXkc=");
+    .RegisterLicense("Mgo+DSMBMAY9C3t2VlhhQlVHfV5AQmBIYVp/TGpJfl96cVxMZVVBJAtUQF1hSn9SdkNjUXxadXVSTmNY");
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
 
